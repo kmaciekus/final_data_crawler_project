444bd6a89b13bf31a5674f88a67af141bea4a7fb
KM <kazimieras.maciekus@gmail.com>
2024-01-07 15:20:24 +0200

    added documentation, fixed naming

--- final_data_crawler_project/utils/converters.py
+++ final_data_crawler_project/utils/converters.py
@@ -1,4 +1,16 @@
 def convert_to_float(value):
+    """
+    Converts a string to a float.
+
+    Parameters:
+        - value: The input string to be converted.
+
+    Returns:
+        - float or None: The converted float value if successful, or None if the conversion fails.
+
+    Notes:
+        - If the input value is an empty string, None is returned.
+    """
     if value == "":
         return None 
     else:
@@ -9,6 +21,18 @@ def convert_to_float(value):
             return None
 
 def convert_to_int(value):
+    """
+    Converts a string to an integer.
+
+    Parameters:
+        - value: The input string to be converted.
+
+    Returns:
+        - int or None: The converted integer value if successful, or None if the conversion fails.
+
+    Notes:
+        - If the input value is an empty string, None is returned.
+    """
     if value == "":
         return None 
     else:
